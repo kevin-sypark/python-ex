e533627668bf49daf7f90745ff7428dc2238631c
Kevin S. Park <sypark75@Kevins-MacBook.local>
2021-02-09 08:22:23 +0900

    add string 6

--- first_script.py
+++ first_script.py
@@ -85,3 +85,5 @@ string5_replace = string5.replace(" ","!@!")
 print("Output #32: (with !@!): {0:s}".format(string5_replace))
 string5_replace = string5.replace(" ", ",")
 print("Output #33 (with commas): {0:s}".format(string5_replace))
+
+string6 = "Here's WHAT Happens WHEN You Use lower."
