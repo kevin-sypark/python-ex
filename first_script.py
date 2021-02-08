552d07a688a2c458418f445eac9fe17874eb191e
Kevin S. Park <sypark75@Kevins-MacBook.local>
2021-02-09 08:46:12 +0900

    fisish array

--- first_script.py
+++ first_script.py
@@ -89,4 +89,10 @@ print("Output #33 (with commas): {0:s}".format(string5_replace))
 string6 = "Here's WHAT Happens WHEN You Use lower."
 print("Output #34: {0:s}".format(string6.lower()))
 string7 = "Here's what Happens when You Use UPPER."
-
+print("Output #35: {0:s}".format(string7.upper()))
+string5 = "here's WHAT Happens WHEN you use Capitalize."
+print("Output #36: {0:s}".format(string5.capitalize()))
+string5_list = string5.split()
+print("Output #37 (on each word):")
+for word in string5_list:
+    print("{0:s}".format(word.capitalize()))
