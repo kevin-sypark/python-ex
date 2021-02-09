51ef88f231ab9bc80ac34c54aa81614628322e8c
Kevin S. Park <sypark75@Kevins-MacBook.local>
2021-02-09 23:05:07 +0900

    create dictionary_script.py

---
+++ dictionary_script.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+from math import exp, log, sqrt
+import re
+from datetime import date, time, datetime, timedelta
+from operator import itemgetter
+
+empty_dict = {}
+a_dict = {'one':1, 'two':2, 'three':3}
+print("Output #102: {}".format(a_dict))
+print("Output #103: a_dict has {!s} elements".format(len(a_dict)))
+another_dict = {'x':'printer', 'y':5, 'z':['star', 'circle', 9]}
+print("Output #104: {}".format(another_dict))
+print("Output #105: another_dict also has {!s} elements".format(len(another_dict)))
