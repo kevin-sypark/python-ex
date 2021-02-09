cc759800271bd9a7258f7040ccd31c788ddefa36
Kevin S. Park <sypark75@Kevins-MacBook.local>
2021-02-09 23:30:04 +0900

    final dictionary_script.py

--- dictionary_script.py
+++ dictionary_script.py
@@ -12,3 +12,32 @@ print("Output #103: a_dict has {!s} elements".format(len(a_dict)))
 another_dict = {'x':'printer', 'y':5, 'z':['star', 'circle', 9]}
 print("Output #104: {}".format(another_dict))
 print("Output #105: another_dict also has {!s} elements".format(len(another_dict)))
+
+print("Output #106: {}".format(a_dict['two']))
+print("Output #107: {}".format(another_dict['z']))
+
+a_new_dict = a_dict.copy()
+print("Output #108: {}".format(a_new_dict))
+
+print("Output #109: {}".format(a_dict.keys()))
+print("Output #110: {}".format(a_dict.values()))
+print("Output #111: {}".format(a_dict.items()))
+
+if 'y' in another_dict:
+    print("Output #114: y is a key in another_dict: {}.".format(another_dict.keys()))
+if 'c' not in another_dict:
+    print("Output #115: c is not a key in another_dict: {}.".format(another_dict.keys()))
+print("Output #116: {!s}".format(a_dict.get('three')))
+print("Output #117: {!s}".format(a_dict.get('four')))
+print("Output #118: {!s}".format(a_dict.get('four', 'Not in dict')))
+
+print("Output #119: {}".format(a_dict))
+dict_copy = a_dict.copy()
+ordered_dict1 = sorted(dict_copy.items(),key=lambda item: item[0])
+print("Output #120 (order by keys): {}".format(ordered_dict1))
+ordered_dict2 = sorted(dict_copy.items(), key=lambda item: item[1])
+print("Output #121 (order by values): {}".format(ordered_dict2))
+ordered_dict3 = sorted(dict_copy.items(), key=lambda x: x[1], reverse=True)
+print("Output #122 (ordered by values, descending): {}".format(ordered_dict3))
+ordered_dict4 = sorted(dict_copy.items(), key=lambda x: x[1], reverse=False)
+print("Output #123 (ordered by values, ascending): {}".format(ordered_dict4))
