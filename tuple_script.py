7d991cced05860389d42c18e1ea0f273ec592a9b
Kevin S. Park <sypark75@Kevins-MacBook.local>
2021-02-09 22:56:26 +0900

    modify tuple_script.py

---
+++ tuple_script.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+from math import exp, log, sqrt
+import re
+from datetime import date, time, datetime, timedelta
+from operator import itemgetter
+
+my_tuple = ('x', 'y', 'z')
+print("Output #93: {}".format(my_tuple))
+print("Output #94: my_tuple has {} elements".format(len(my_tuple)))
+print("Output #95: {}".format(my_tuple[1]))
+longer_tuple = my_tuple + my_tuple
+print("Output #96: {}".format(longer_tuple))
