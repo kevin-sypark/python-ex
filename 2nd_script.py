129b616020c6b36bc34dee9853bd44f699cca37b
Kevin S. Park <sypark75@Kevins-MacBook.local>
2021-02-09 17:24:43 +0900

    end 2nd_script.py

--- 2nd_script.py
+++ 2nd_script.py
@@ -11,3 +11,18 @@ for word in string_list:
     if pattern.search(word):
         count += 1
 print("Output #38: {0:d}".format(count))
+
+# 문자열 내에서 발견된 패턴 출력하기
+string = "The quick brown for jumps over the lazy dog."
+string_list = string.split()
+pattern = re.compile(r"(?P<match_word>The)", re.I)
+print("output #39:")
+for word in string_list:
+    if pattern.search(word):
+        print("{0:s}".format(pattern.search(word).group('match_word')))
+
+# 문자열 내 "the"를 "a"로 대체하기
+string = "The quick brown fox jumps over the lazy dog."
+string_to_find = r"The"
+pattern = re.compile(string_to_find, re.I)
+print("Outout #40: {:s}".format(pattern.sub("a", string)))
