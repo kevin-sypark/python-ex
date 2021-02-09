a903b87ffac4904cbbd88c9937af8801a06df2a1
Kevin S. Park <sypark75@Kevins-MacBook.local>
2021-02-09 22:56:57 +0900

    final tuple_script.py

--- tuple_script.py
+++ tuple_script.py
@@ -11,3 +11,16 @@ print("Output #94: my_tuple has {} elements".format(len(my_tuple)))
 print("Output #95: {}".format(my_tuple[1]))
 longer_tuple = my_tuple + my_tuple
 print("Output #96: {}".format(longer_tuple))
+
+one, two, three = my_tuple
+print("Output #97: {0} {1} {2}".format(one, two, three))
+var1 = 'red'
+var2 = 'robin'
+print("Output #98: {} {}".format(var1, var2))
+var1, var2 = var2, var1
+print("Output #99: {} {}".format(var1, var2))
+
+my_list = [1,2,3]
+my_tuple = ('x', 'y', 'z')
+print("Output #100: {}".format(tuple(my_list)))
+print("Output #101: {}".format(list(my_tuple)))
