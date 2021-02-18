d74627aedba9f560d7ba042a3ff560a35ff0adac
Kevin S. Park <sypark75@Kevins-MacBook.local>
2021-02-18 21:24:07 +0900

    control script

--- control_script.py
+++ control_script.py
@@ -40,3 +40,23 @@ another_dict = {'x':'printer', 'y':5, 'z':['star', 'circle', 9]}
 print("Output #129:")
 for key, value in another_dict.items():
     print("{0:s}, {1}".format(key, value))
+
+my_data = [[1,2,3],[4,5,6],[7,8,9]]
+rows_to_keep = [row for row in my_data if row[2] > 5]
+print("Output #130 (list comprehension): {}".format(rows_to_keep))
+
+my_data = [(1,2,3),(4,5,6),(7,8,9),(7,8,9)]
+set_of_tuples1 = {x for x in my_data}
+print("Output #131 (set comprehension): {}".format(set_of_tuples1))
+set_of_tuples2 = set(my_data)
+print("Output #132 (set function): {}".format(set_of_tuples2))
+
+my_dictionary = {'customer1': 7, 'customer2': 9, 'customer3': 11}
+my_results = {key : value for key, value in my_dictionary.items() if value > 10}
+print("Output #133 (dictionary comprehension): {}".format(my_results))
+
+print("Output #134:")
+x = 0
+while x < 11:
+    print("{!s}".format(x))
+    x += 1
