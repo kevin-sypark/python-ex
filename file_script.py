edf929b65f59b7b815b14644cd5c75091c84f5b5
Kevin S. Park <kevin.sypark@gmail.com>
2021-02-18 06:34:40 +0000

    finish file_script

--- file_script.py
+++ file_script.py
@@ -22,10 +22,35 @@ import os
 #    for row in filereader:
 #        print("{}".format(row.strip()))
 
-print("Output #141:")
-inputPath = sys.argv[1]
-for input_file in glob.glob(os.path.join(inputPath, '*.txt')):
-    with open(input_file, 'r', newline='') as filereader:
-        for row in filereader:
-            print("{}".format(row.strip()))
+#print("Output #141:")
+#inputPath = sys.argv[1]
+#for input_file in glob.glob(os.path.join(inputPath, '*.txt')):
+#    with open(input_file, 'r', newline='') as filereader:
+#        for row in filereader:
+#            print("{}".format(row.strip()))
+
+#my_letters = ['a','b','c','d','e','f','g','h','i','j']
+#max_index = len(my_letters)
+#output_file = sys.argv[1]
+#filewriter = open(output_file, 'w')
+#for index_value in range(len(my_letters)):
+#    if index_value < (max_index-1):
+#        filewriter.write(my_letters[index_value]+'\t')
+#    else:
+#        filewriter.write(my_letters[index_value]+'\n')
+#filewriter.close()
+#print("Output #142: Output written to file")
+
+my_numbers = [0,1,2,3,4,5,6,7,8,9]
+max_index = len(my_numbers)
+output_file = sys.argv[1]
+filewriter = open(output_file, 'a')
+for index_value in range(len(my_numbers)):
+    if index_value < (max_index-1):
+        filewriter.write(str(my_numbers[index_value])+',')
+    else:
+        filewriter.write(str(my_numbers[index_value])+'\n')
+filewriter.close()
+print("Output #143: Output appended to file")
+
 
